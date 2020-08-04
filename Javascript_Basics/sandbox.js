5b932fc4f4df9f8a919888847f58a45ffced7a83
Renuja99 <devmindadeco@gmail.com>
2020-08-04 16:25:19 +0530

    First javascript file

---
+++ Javascript_Basics/sandbox.js
@@ -0,0 +1,35 @@
+// 2.1 VARIABLES
+
+let age = 21;
+let year = 1999;
+
+console.log(age,year);
+
+age = 22;
+
+console.log(age);
+
+//2.2 CONSTANTS 
+
+const id = 01;
+console.log(id);
+
+// id = 02;
+// console.log(id);//Assignment to constant variable. Therefore displays an error.
+
+// shortcut for commenting Ctrl + /      =>    Ctrl + (forward slash)
+
+//2.3 STRINGS
+
+let email = 'devmindadeco@gmail.com';
+console.log(email);
+
+    //2.3.1 string concatenation
+
+    let firstName = 'Brandon';
+    let lastName = 'Anderson';
+
+    let fullName = firstName + ' ' + lastName;
+    console.log(fullName);
+
+
